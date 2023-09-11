b0af9e6cc92c041057d8f333bcc4bded3c7e7cdf
Will Pogge <william.d.pogge@nasa.gov>
2023-09-11 11:31:16 -0400

    Update the majority vote examples.

--- TutorialAndDevGuide/Tutorial/Examples/Filters.hs
+++ TutorialAndDevGuide/Tutorial/Examples/Filters.hs
@@ -12,16 +12,13 @@ diff :: Stream Int32
 diff = fib - fibdelayed
 
 gain :: Stream Int32
-gain = fib + 2 
+gain = fib * 2 
 
 twoTermAverage :: Stream Int32
 twoTermAverage =  (fib + fibdelayed) `div` 2
 
 centralDifference :: Stream Int32
 centralDifference = (fib - ([0] ++ fibdelayed)) `div` 2
-
-recursive :: Stream Int32
-recursive = fib - diff
 
 gainFilter = do 
   observer "fib" fib
@@ -31,7 +28,6 @@ diffFilter = do
   observer "fib" fib
   observer "fib delayed" fibdelayed
   observer "Difference Filter" diff
-  observer "Recursive Diff" recursive
 
 pureDelay = do 
   observer "fib" fib
--- TutorialAndDevGuide/Tutorial/Examples/Spec.hs
+++ TutorialAndDevGuide/Tutorial/Examples/Spec.hs
@@ -5,7 +5,7 @@ import qualified Prelude as P
 nats :: Stream Int32
 nats = [0] ++ (1 + nats)
 
-evenNumberNumber :: Stream Int32 -> Stream Bool
+evenNumber :: Stream Int32 -> Stream Bool
 evenNumber n = n `mod` 2 == 0
 
 oddNumber :: Stream Int32 -> Stream Bool
